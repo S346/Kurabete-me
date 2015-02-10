f95cb9de44e5e2f8e6c19aaf1b021bf0e96a0623
S346 <Sanshiro@SSMBP.local>
2015-02-10 14:34:17 +0900

    Initial Commit

---
+++ Kurabete-me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kurabete-me
+//
+//  Created by 坂井三四郎 on 2015/02/10.
+//  Copyright (c) 2015年 Sanshiro Sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
